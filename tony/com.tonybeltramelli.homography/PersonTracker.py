b34886a573371212e501fded9451f270718a3d1b
Tony Beltramelli <tbeltramelli@gmail.com>
2015-03-26 15:04:12 +0100

    update tracking and add homography computation function from mouse input

--- tony/com.tonybeltramelli.homography/PersonTracker.py
+++ tony/com.tonybeltramelli.homography/PersonTracker.py
@@ -6,17 +6,25 @@ from Filtering import *
 class PersonTracker:
     _result = None
     _data = None
+    _map = None
     _counter = 0
 
     def __init__(self, video_path, map_path, tracking_data_path):
         self._data = self._get_tracking_data(tracking_data_path)
+        self._map = UMedia.get_image(map_path)
 
         UMedia.load_media(video_path, self.process)
 
     def process(self, img):
         self._result = img
-        img = Filtering.apply_box_filter(Filtering.get_gray_scale_image(img), 3)
 
+        #homography, points = self._get_homography_from_mouse([img, self._map])
+
+        #A = array([[points[0]])
+
+        self._get_person_position()
+
+    def _get_person_position(self):
         self._counter += 1
 
         row = self._data[self._counter]
@@ -25,7 +33,15 @@ class PersonTracker:
         for i, box in enumerate(row):
             cv2.rectangle(self._result, box[0], box[1], colors[i])
 
+        box = row[len(row)-1]
+        base_x = box[0][0] + ((box[1][0] - box[0][0]) / 2)
+        base_y = box[1][1]
+
+        cv2.circle(self._result, (base_x, base_y), 1,  (0, 255, 255))
+
         UMedia.show(self._result)
+
+        return base_x, base_y
 
     def _get_tracking_data(self, tracking_data_path):
         data = np.loadtxt(tracking_data_path)
@@ -45,3 +61,33 @@ class PersonTracker:
             box = tuple(points[i:i + 2])
             boxes.append(box)
         return boxes
+
+    def _get_homography_from_mouse(self, images, n=4):
+        image_points = []
+        fig = figure(1)
+
+        for i, img in enumerate(images):
+            img = copy(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+            ax = subplot(1, 2, i + 1)
+            ax.imshow(img)
+
+            title("Select " + str(n) + " points.")
+
+            fig.canvas.draw()
+            ax.hold('On')
+
+            image_points.append(fig.ginput(n, -1))
+
+            for p in image_points[i]:
+                cv2.circle(img, (int(p[0]), int(p[1])), 2, (0, 255, 0), 2)
+
+            ax.imshow(img)
+
+        close(fig)
+
+        points1 = np.array([[x, y] for (x, y) in image_points[0]])
+        points2 = np.array([[x, y] for (x, y) in image_points[len(image_points) - 1]])
+
+        homography, mask = cv2.findHomography(points1, points2)
+        return homography, image_points
