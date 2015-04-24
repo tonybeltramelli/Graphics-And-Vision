a43f9fefce8ca36a441bc948b3f993a01d312aee
Tony Beltramelli <tbeltramelli@gmail.com>
2015-04-24 14:18:22 +0200

    new version fundamental matrix

--- tony/com.tonybeltramelli.stereo/StereoVision.py
+++ tony/com.tonybeltramelli.stereo/StereoVision.py
@@ -1,6 +1,6 @@
 __author__ = 'tbeltramelli'
 
-from FundamentalMatrix import *
+from EpipolarGeometry import *
 
 
 class StereoVision:
@@ -9,18 +9,10 @@ class StereoVision:
     def __init__(self, left_video_path, right_video_path):
         self._result = self.get_stereo_images(UMedia.get_frame_from_video(left_video_path, 1), UMedia.get_frame_from_video(right_video_path, 1))
 
-        fm = FundamentalMatrix()
-        m = fm.get(self._result)
-
-        #self.compute_epipole(matrix)
+        fm = EpipolarGeometry(self._result)
 
     def get_stereo_images(self, raw_left, raw_right):
         left = cv2.pyrDown(raw_left)
         right = cv2.pyrDown(raw_right)
 
         return UMedia.combine_images(left, right)
-
-    def compute_epipole(self, F):
-        U, S, V = linalg.svd(F)
-        e = V[-1]
-        return e/e[2]
