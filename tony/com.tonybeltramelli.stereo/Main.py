ed59b50b83b47ff883c0f3ea8a3760117980f6ae
Tony Beltramelli <tbeltramelli@gmail.com>
2015-04-29 16:16:07 +0200

    save calibration image

--- tony/com.tonybeltramelli.stereo/Main.py
+++ tony/com.tonybeltramelli.stereo/Main.py
@@ -1,20 +1,5 @@
 __author__ = 'tbeltramelli'
 
-#from StereoVision import *
+from StereoVision import *
 
-#st = StereoVision("../data/media/cameraLeft.mov", "../data/media/cameraRight.mov", "../data/output")
-
-from StereoCameraCalibrator import *
-
-left_img1 = cv2.pyrDown(UMedia.get_frame_from_video("../data/media/cameraLeft.mov", 20))
-right_img1 = cv2.pyrDown(UMedia.get_frame_from_video("../data/media/cameraRight.mov", 20))
-
-left_img2 = cv2.pyrDown(UMedia.get_frame_from_video("../data/media/cameraLeft.mov", 50))
-right_img2 = cv2.pyrDown(UMedia.get_frame_from_video("../data/media/cameraRight.mov", 50))
-
-#UMedia.show(left_img1, right_img1, left_img2, right_img2)
-#UInteractive.pause("pause")
-
-calibrator = StereoCameraCalibrator(20)
-calibrator.calibrate(left_img1, right_img1)
-calibrator.calibrate(left_img2, right_img2)
+st = StereoVision("../data/media/cameraLeft2.mov", "../data/media/cameraRight2.mov", "../data/output")
