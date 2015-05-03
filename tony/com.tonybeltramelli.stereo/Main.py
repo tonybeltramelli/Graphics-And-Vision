56f90675049bab87bbf6329fb1dc7ba2cd97a899
Tony Beltramelli <tbeltramelli@gmail.com>
2015-05-03 22:18:51 +0200

    finalize depth map

--- tony/com.tonybeltramelli.stereo/Main.py
+++ tony/com.tonybeltramelli.stereo/Main.py
@@ -3,7 +3,7 @@ __author__ = 'tbeltramelli'
 from StereoVision import *
 
 st = StereoVision("../data/media/", "../data/output/")
-#st.epipolar_geometry("cameraLeft.mov", "cameraRight.mov", 11)
+st.epipolar_geometry("cameraLeft.mov", "cameraRight.mov", 11)
 st.stereo_vision_from_images([25, 37, 74, 80, 113], [152, 172, 182, 186, 202, 204, 252, 269])
-#st.stereo_vision_from_video("cameraLeft2.mov", "cameraRight2.mov")
+st.stereo_vision_from_video("cameraLeft2.mov", "cameraRight2.mov")
 
